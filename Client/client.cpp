f4f167913b3db1c20878469b8af55e1367cc5674
Sv163Sam <Tankruuus@gmail.com>
2024-10-13 19:23:51 +0400

    Push

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,58 +1,70 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <libssh2.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main() {
-    // Инициализация libssh2
-    libssh2_init(0);
-
-    // Создание сессии
-    LIBSSH2_SESSION *session = libssh2_session_init();
-    if (!session) {
-        std::cerr << "Не удалось инициализировать сессию" << std::endl;
-        return -1;
-    }
-
-    // Установка соединения с сервером
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in server_addr = {};
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080); // Порт сервера
-    inet_pton(AF_INET, "192.168.31.250", &server_addr.sin_addr); // Замените на IP сервера
-
-    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
-        std::cerr << "Ошибка подключения к серверу" << std::endl;
-        close(sock);
-        return -1;
-    }
-    std::cout << "1";
-    // Установка SSH-соединения
-    libssh2_session_handshake(session, sock);
-    std::cout << "2";
-    // Аутентификация
-    const char *username = "Alice"; // Замените на ваше имя пользователя
-    const char *password = "Alice"; // Замените на ваш пароль
-
-    if (libssh2_userauth_password(session, username, password) != 0) {
-        std::cerr << "Ошибка аутентификации: " << libssh2_session_last_error(session, nullptr, nullptr, 0) << std::endl;
-        libssh2_session_free(session);
-        close(sock);
-        return -1;
-    }
-
-    std::cout << "Успешно подключено!" << std::endl;
-
-    // Здесь можно выполнять команды или обрабатывать дальнейшие действия
-
-    // Закрытие сессии и сокета
-    libssh2_session_disconnect(session, "Bye");
-    libssh2_session_free(session);
-    close(sock);
-    libssh2_exit();
-
-    return 0;
+void init_openssl() {
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
 }
+
+void cleanup_openssl() {
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context() {
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_client_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        std::cout << stderr;
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+int main() {
+    init_openssl();
+    SSL_CTX *ctx = create_context();
+
+    SSL *ssl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4433);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int res = connect(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, server_fd);
+
+    if (SSL_connect(ssl) <= 0) {
+        std::cout << stderr;
+    }
+    else
+    {
+        std::string message = "Hello from client!";
+        SSL_write(ssl, message.c_str(), (int)message.size());
+
+        char buffer[1024] = {0};
+        int bytes = SSL_read(ssl, buffer, sizeof(buffer));
+        buffer[bytes] = 0;
+        std::cout << "Received: " << buffer << std::endl;
+    }
+
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    close(server_fd);
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
+}
+
+
--- Server/server.cpp
+++ Server/server.cpp
@@ -1,86 +1,73 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <libssh2.h>
 
-#define PORT 8080 // Порт для сервера
+void init_openssl() {
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_openssl() {
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context() {
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        std::cout << stderr;
+        exit(EXIT_FAILURE);
+    }
+
+    SSL_CTX_use_certificate_file(ctx, "../Keys/server.crt", SSL_FILETYPE_PEM);
+    SSL_CTX_use_PrivateKey_file(ctx, "../Keys/server.key", SSL_FILETYPE_PEM);
+
+    return ctx;
+}
 
 int main() {
-    // Инициализация libssh2
-    libssh2_init(0);
+    init_openssl();
+    SSL_CTX *ctx = create_context();
 
-    // Создание сокета
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        std::cerr << "Ошибка при создании сокета" << std::endl;
-        return -1;
-    }
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4433);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Настройка адреса
-    sockaddr_in server_addr = {};
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    int res = bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 1);
 
-    // Привязка сокета к адресу
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Ошибка при привязке сокета" << std::endl;
-        close(server_socket);
-        return -1;
-    }
+    while(true) {
+        SSL *ssl;
+        int client_fd = accept(server_fd, nullptr, nullptr);
+        ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client_fd);
 
-    // Начинаем слушать входящие соединения
-    listen(server_socket, 5);
-    std::cout << "Сервер запущен и слушает на порту " << PORT << std::endl;
+        if (SSL_accept(ssl) <= 0) {
+            std::cout << stderr;
+        } else {
+            char buffer[1024] = {0};
+            int bytes = SSL_read(ssl, buffer, sizeof(buffer));
+            buffer[bytes] = 0;
+            std::cout << "Received: " << buffer << std::endl;
 
-    while (true) {
-        int client_socket = accept(server_socket, nullptr, nullptr);
-        if (client_socket >= 0) {
-            std::cout << "Клиент подключился!" << std::endl;
-
-            // Обработка SSH соединения
-            if(libssh2_session_init() != nullptr)
-                std::cout << "NULL";
-
-            LIBSSH2_SESSION *session = libssh2_session_init();
-            std::cout << "0";
-            if (!session) {
-                std::cerr << "Не удалось инициализировать сессию" << std::endl;
-                close(client_socket);
-                continue;
-            }
-            std::cout << "1";
-            // Установка соединения
-            libssh2_session_handshake(session, client_socket);
-            std::cout << "2";
-            // Аутентификация (например, по паролю)
-            const char *username = "Alice"; // Замените на ваше имя пользователя
-            const char *password = "Alice"; // Замените на ваш пароль
-
-            if (libssh2_userauth_password(session, username, password) != 0) {
-                std::cerr << "Ошибка аутентификации: " << libssh2_session_last_error(session, nullptr, nullptr, 0) << std::endl;
-                libssh2_session_disconnect(session, "Bye");
-                libssh2_session_free(session);
-                close(client_socket);
-                continue;
-            }
-
-            std::cout << "Клиент успешно аутентифицирован!" << std::endl;
-
-            // Здесь можно выполнять команды или обрабатывать дальнейшие действия
-
-            // Закрытие сессии и сокета
-            libssh2_session_disconnect(session, "Bye");
-            libssh2_session_free(session);
-            close(client_socket);
+            std::string reply = "Message received!";
+            SSL_write(ssl, reply.c_str(), (int)reply.size());
         }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(client_fd);
     }
 
-    // Закрытие сервера
-    close(server_socket);
-    libssh2_exit();
-    return 0;
+    close(server_fd);
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
 }
